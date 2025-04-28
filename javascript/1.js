de7c27eb3e833eb41d0649dcb675dbbd963b5baf
ritul <rituldingora1@gmail.com>
2025-04-28 11:50:09 +0530

    project done

--- javascript/1.js
+++ javascript/1.js
@@ -1,2 +1,53 @@
-var x=10;
-console.log(x)
+// var x=10;
+// var y = "10"
+// console.log(typeof(x));
+// console.log(typeof(y));
+// console.log(x === y);
+// console.log("hiiiiiiiiiiii");
+// console.log(x == y);
+
+
+// let arr1 = [1,2,4]
+// let arr2 = [1,2,4]
+// console.log(arr1 == arr2);
+// let arr3 = arr1
+// console.log(arr1 == arr3);
+// console.log(arr1 === arr2);
+// console.log(arr1 === arr3);
+
+// greet(); // calling the function -> prints: Hello, world!
+// greet(); // calling the function -> prints: Hello, world!
+// greet(); // calling the function -> prints: Hello, world!
+
+// function greet() {
+//   console.log("Hello, world! iohugiyfutgvuyjbihnjumkijlkhugyjxzdfs");
+// }
+
+// function add(a, b) {
+//   return a + b;
+// }
+
+// // const result = add(5, 3); // result = 8
+// console.log(add(4,7));
+
+// // console.log(result);      // prints: 8
+
+
+const sayHi = function() {
+  console.log("Hiiiiiiiiiiiiiii!");
+};
+
+// console.log(sayHi);
+
+// console.log(sayHi());
+sayHi()
+
+
+
+
+
+
+
+
+
+// console.log(x)
